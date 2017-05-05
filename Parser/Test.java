0cf304dc00df3d02b62edc5358db127585d4e221
armysick <gustpint@gmail.com>
2017-05-05 12:42:43 +0100

    ivy + Test file

--- Parser/Test.java
+++ Parser/Test.java
@@ -2,10 +2,17 @@ public class Test {
 
     public static void main(String[] args) {
 		int acc = 2;
-		int STEP;
+		int STEP = 0;
         //@pragma tuner explore STEP(1, 10) reference(STEP=1)
-        acc *= STEP;
+		for(int x = 0 ; x < 10000 ; x++){
+			for(int i = 0; i < Math.exp(STEP) ; i++){
+				acc += STEP;
+			}
+		}
 		//@pragma tuner max_abs_error acc 5
+		
+		int a = acc;
+		
     }
 }
 
